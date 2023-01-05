aac4b40e8615f60105bd7a0cbcea4ed44c3697a1
Mon Asuncion <raymond.s.asuncion@smretail.com>
2023-01-05 14:00:34 +0800

    Test GithubActions

---
+++ scripts/create-identity-pool.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+export PROJECT_ID="secret-medium-373003"
+export IDENTITY_POOL="googlemon-pool4"
+export IDENTITY_PROVIDER="googlemon-identity-provider"
+export SERVICE_ACCOUNT="googlemon-service-account"
+export REPO="smic-ops/smri-gcp-terraform"
+
+gcloud iam service-accounts create $SERVICE_ACCOUNT \
+  --project "${PROJECT_ID}"
+
+gcloud services enable iamcredentials.googleapis.com \
+  --project "${PROJECT_ID}"
+
+gcloud iam workload-identity-pools create $IDENTITY_POOL \
+  --project="${PROJECT_ID}" \
+  --location="global" \
+  --display-name="$IDENTITY_POOL"
+
+WORKLOAD_IDENTITY_POOL_ID=$(gcloud iam workload-identity-pools describe $IDENTITY_POOL \
+  --project="${PROJECT_ID}" \
+  --location="global" \
+  --format="value(name)") 
+
+gcloud iam workload-identity-pools providers create-oidc $IDENTITY_PROVIDER \
+  --project="${PROJECT_ID}" \
+  --location="global" \
+  --workload-identity-pool=$IDENTITY_POOL \
+  --display-name="$IDENTITY_POOL provider" \
+  --attribute-mapping="google.subject=assertion.sub,attribute.actor=assertion.actor,attribute.repository=assertion.repository" \
+  --issuer-uri="https://token.actions.githubusercontent.com"
+
+gcloud iam service-accounts add-iam-policy-binding "${SERVICE_ACCOUNT}@${PROJECT_ID}.iam.gserviceaccount.com" \
+  --project="${PROJECT_ID}" \
+  --role="roles/iam.workloadIdentityUser" \
+  --member="principalSet://iam.googleapis.com/${WORKLOAD_IDENTITY_POOL_ID}/attribute.repository/${REPO}"
+
+gcloud iam workload-identity-pools providers describe $IDENTITY_PROVIDER \
+  --project="${PROJECT_ID}" \
+  --location="global" \
+  --workload-identity-pool=$IDENTITY_POOL \
+  --format="value(name)"
